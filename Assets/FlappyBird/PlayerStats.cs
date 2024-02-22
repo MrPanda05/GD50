e601a73dd2d4be9adec3bb609b866755f89f92ca
MrPanda05 <Buttercup98Cool@protonmail.com>
2024-02-22 17:51:52 -0300

    .
    .

--- Assets/FlappyBird/PlayerStats.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName="BirdStats")]
-public class PlayerStats : ScriptableObject
-{
-    public int points = 0;
-}
